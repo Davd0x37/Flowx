b4fe4cb89e69f830467f2bcdbec2035c9afd631b
David <David>
2023-10-06 14:32:44 +0200

    feature test

--- apps/web/src/common/features.ts
+++ apps/web/src/common/features.ts
@@ -1,32 +1,56 @@
-import { RouteRecordRaw } from 'vue-router';
+export type FeatureName = PropertyKey;
 
-export interface FeatureLocales {
-  [key: PropertyKey]: Record<PropertyKey, unknown>;
-}
-
-export type FeatureRoutes = RouteRecordRaw;
-
-export type FeatureStore<T> = T;
-
-type FeatureName = string;
-
-export interface Feature<StoreType> {
-  [key: PropertyKey]: unknown;
-  locales: FeatureLocales;
-  routes: FeatureRoutes;
-  store: StoreType;
-}
-
-const featuresStore: Map<FeatureName, Feature<any>> = new Map();
-
-export const getFeatures = () => featuresStore;
-
-export const getFeature = (featureName: FeatureName) => featuresStore.get(featureName);
-
-export const installFeature = <StoreType>(featureName: FeatureName, feature: Feature<StoreType>) => {
-  if (featuresStore.has(featureName)) return;
-
-  featuresStore.set(featureName, feature);
-
-  return feature;
+export type Feature<Type> = {
+  [Key in keyof Type]: Type[Key] extends (...args: any[]) => any
+    ? (...args: Parameters<Type[Key]>) => ReturnType<Type[Key]>
+    : Type[Key];
 };
+
+export type FeatureType<T> = {
+  name: string;
+  feature: T;
+};
+
+export type FeatureStore<T extends Feature<unknown>> = Map<FeatureName, T>;
+
+let User = {
+  name: 'user',
+  getUser: () => {
+    return User.name;
+  },
+  setUser: (user: string) => {
+    User.name = user;
+  },
+};
+
+const featureStore: FeatureStore<Feature<unknown>> = new Map();
+
+featureStore.set('user', {
+  name: 'user',
+  feature: User,
+});
+
+function installFeature<T extends Feature<unknown>>(feature: FeatureType<T>) {
+  featureStore.set(feature.name, feature);
+}
+
+// type MapKeys<T> = T extends Map<infer K, any> ? K : never;
+
+function getFeature<T extends Feature<unknown>>(featureName: FeatureName) {
+  return featureStore.get(featureName)
+}
+
+installFeature({ name: 'user', feature: User });
+
+type MapKeys<T> = T extends Map<infer K, any> ? K : never;
+type MapValues<T> = T extends Map<any, infer V> ? V : never;
+const testxd: MapKeys<typeof featureStore> = {}
+type MapType<K extends MapKeys<typeof featureStore>, V extends MapValues<typeof featureStore>> = {
+  [key in K]: V;
+};
+
+const mapType: MapType<MapKeys<typeof featureStore>, MapValues<typeof featureStore>> = {
+
+};
+
+let xd = getFeature('name')
