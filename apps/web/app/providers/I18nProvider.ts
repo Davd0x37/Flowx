b61ff531e66d9d4b88a74c4726aaf55749050796
Davd0x37 <57641574+Davd0x37@users.noreply.github.com>
2024-05-25 20:08:15 +0200

    fix(web): remove top-level await i18n

--- apps/web/app/providers/I18nProvider.ts
+++ apps/web/app/providers/I18nProvider.ts
@@ -3,15 +3,18 @@ import { SupportedLocales } from '@/config/constants';
 import locales from '@/locales';
 import i18n from 'i18next';
 
-await i18n.use(initReactI18next).init({
-  resources: locales,
-  lng: 'pl',
-  fallbackLng: 'en',
-  supportedLngs: SupportedLocales,
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: locales,
+    lng: 'pl',
+    fallbackLng: 'en',
+    supportedLngs: SupportedLocales,
 
-  interpolation: {
-    escapeValue: false,
-  },
-});
+    interpolation: {
+      escapeValue: false,
+    },
+  })
+  .catch(() => {});
 
 export { i18n, I18nextProvider };
