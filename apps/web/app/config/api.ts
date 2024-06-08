0121e5b3e7e161346a9d2a219cb8db00e5417741
Davd0x37 <57641574+Davd0x37@users.noreply.github.com>
2024-06-08 14:20:04 +0200

    refactor(web): change api routes

--- apps/web/app/config/api.ts
+++ apps/web/app/config/api.ts
@@ -1,6 +1,5 @@
-// @TODO: move to shared package. Reuse in api and web apps
-import { API_PREFIX } from './constants';
+import { API_URL } from './constants';
 
-export const userAuthLogin = `${API_PREFIX}/auth/login`;
-export const userAuthLogout = `${API_PREFIX}/auth/logout`;
-export const userAuthSignup = `${API_PREFIX}/auth/signup`;
+export const userAuthLogin = `${API_URL}/auth/login`;
+export const userAuthLogout = `${API_URL}/auth/logout`;
+export const userAuthSignup = `${API_URL}/auth/signup`;
--- apps/web/app/config/constants.ts
+++ apps/web/app/config/constants.ts
@@ -9,4 +9,8 @@ export const StorageThemeKey = 'FlowxStorageThemeKey';
 // Value used in avatar/sidebar component when acronym can't be generated
 export const AVATAR_FALLBACK = 'AV';
 
-export const API_PREFIX = 'http://localhost:3000/api';
+// API Configuration
+export const API_PORT = (import.meta.env.VITE_API_PORT as number) ?? 3000;
+export const API_HOST = (import.meta.env.VITE_API_HOST as string) ?? 'http://localhost';
+export const API_PREFIX = '/api';
+export const API_URL = `${API_HOST}:${API_PORT}${API_PREFIX}`;
